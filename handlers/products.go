a1d3819517496e4da23f50c3652295fe64c7a984
crazyoptimist <crazyoptimist@mail.com>
2022-03-30 21:20:34 +0200

    get data via POST request

--- handlers/products.go
+++ handlers/products.go
@@ -18,9 +18,15 @@ func NewProducts(l *log.Logger) *Products {
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		p.getProducts(rw, r)
-	} else {
-		rw.WriteHeader(http.StatusMethodNotAllowed)
+		return
 	}
+
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
@@ -29,4 +35,14 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to parse json", http.StatusBadRequest)
+	}
+
+	p.l.Printf("Prod: %#v", prod)
 }
