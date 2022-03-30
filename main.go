bcbc61ad7f7794bea9a92b525e87a04591b95a33
crazyoptimist <crazyoptimist@mail.com>
2022-03-30 09:35:58 +0200

    read from request, write to response

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Hello, world")
+		d, _ := ioutil.ReadAll(r.Body)
+		fmt.Fprintf(w, "Hello %s", d)
 	})
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
