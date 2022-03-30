75cb4180b32239453ac8caad5c70a6e479b78d13
crazyoptimist <crazyoptimist@mail.com>
2022-03-30 19:31:06 +0200

    add data layer, add products handler use standard json encoding package for marshaling data

--- main.go
+++ main.go
@@ -13,12 +13,11 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api-", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	hg := handlers.NewGoodbye(l)
+
+	ph := handlers.NewProducts(l)
 
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", hg)
+	sm.Handle("/", ph)
 
 	s := &http.Server{
 		Addr:         ":9090",
