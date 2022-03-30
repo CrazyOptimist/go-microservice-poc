aba294a45c5fecf296bcd952f08fe4220a71e173
crazyoptimist <crazyoptimist@mail.com>
2022-03-30 09:25:29 +0200

    add the first HandleFunc

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+		log.Println("Hello, world")
+	})
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Goodbye, world")
+	})
+
 	http.ListenAndServe(":9090", nil)
 }
